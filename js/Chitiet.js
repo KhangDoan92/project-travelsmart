4165447520366153d24b0cb6eae0eb5d5369575e
SyTuyenDapChai <sytuyentran1011@gmail.com>
2024-09-21 00:06:45 +0700

    change detail

--- js/Chitiet.js
+++ js/Chitiet.js
@@ -51,11 +51,41 @@ document.getElementById('prev').onclick = function(){
     let lists = document.querySelectorAll('.list-item');
     document.querySelector('.lists').prepend(lists[lists.length - 1]);
 }
-setInterval(function(){
-    let lists = document.querySelectorAll('.list-item');
-    document.querySelector('.lists').appendChild(lists[0]);
-}, 2000)
-document.getElementById('moveButton').addEventListener('click', function() {
-    const box = document.getElementById('box');
-    box.classList.toggle('move-down');
+// setInterval(function(){
+//     let lists = document.querySelectorAll('.list-item');
+//     document.querySelector('.lists').appendChild(lists[0]);
+// }, 2000)
+// document.getElementById('moveButton').addEventListener('click', function() {
+//     const box = document.getElementById('box');
+//     box.classList.toggle('move-down');
+// });
+// toggle Change Language
+let A = document.querySelector(".change-language");
+let B = document.querySelector(".changeSub");
+A.addEventListener("click", () =>{
+    B.classList.toggle("change-language-active");
+});
+// language
+document.querySelector('.select-selected').addEventListener('click', function() {
+    this.classList.toggle('active');
+    this.nextElementSibling.classList.toggle('select-hide');
+});
+
+document.querySelectorAll('.select-items div').forEach(item => {
+    item.addEventListener('click', function() {
+        let selected = this.parentNode.previousElementSibling;
+        selected.innerHTML = this.innerHTML + '<span class="caret">▼</span>';
+        selected.classList.remove('active');
+        this.parentNode.classList.add('select-hide');
+    });
+});
+let iconMenuMobile = document.querySelector(".icon-menu-mobile");
+let menuMobile = document.querySelector(".toolbar-header");
+let closeMenu = document.querySelector(".close-toolbar-mobile");
+console.log(closeMenu);
+iconMenuMobile.addEventListener("click", () =>{
+    menuMobile.classList.toggle("change-menu-mobile");
+});
+closeMenu.addEventListener("click", () =>{
+    menuMobile.classList.toggle("change-menu-mobile");
 });
